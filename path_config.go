35d418d9a3d56934cc696e6974a39031658a95ad
Madalyn Parker <madalyn@hashicorp.com>
2019-06-19 15:58:25 -0400

    add forgotten comma

--- path_config.go
+++ path_config.go
@@ -35,11 +35,11 @@ func pathConfig(b *jwtAuthBackend) *framework.Path {
 				Description: "The OAuth Client ID configured with your OIDC provider.",
 			},
 			"oidc_client_secret": {
-				Type:             framework.TypeString,
-				Description:      "The OAuth Client Secret configured with your OIDC provider.",
+				Type:        framework.TypeString,
+				Description: "The OAuth Client Secret configured with your OIDC provider.",
 				DisplayAttrs: &framework.DisplayAttributes{
 					Sensitive: true,
-				}
+				},
 			},
 			"jwks_url": {
 				Type:        framework.TypeString,
