e8878a3269061569431456e06f02992f550c9626
Austin Gebauer <agebauer@hashicorp.com>
2020-07-17 13:45:03 -0700

    adds tests for user and group recursion logic in search method

--- provider_gsuite_test.go
+++ provider_gsuite_test.go
@@ -1,14 +1,216 @@
 package jwtauth
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	admin "google.golang.org/api/admin/directory/v1"
+	"google.golang.org/api/option"
 )
 
 const (
 	serviceAccountKeyJSON = `{"type": "service_account"}`
 )
+
+// Tests the user and group recursion logic in the search method.
+func TestGSuiteProvider_search(t *testing.T) {
+	groupsServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m, _ := url.ParseQuery(r.URL.RawQuery)
+		switch m["userKey"][0] {
+		case "user1":
+			w.Write([]byte(`{
+				"kind": "admin#directory#groups",
+				"groups": [{
+					"kind": "admin#directory#group",
+					"email": "group1@group.com"
+				}]
+			}`))
+		case "group1@group.com":
+			w.Write([]byte(`{
+				"kind": "admin#directory#groups",
+				"groups": [{
+					"kind": "admin#directory#group",
+					"email": "group2@group.com"
+				}]
+			}`))
+		case "group2@group.com":
+			w.Write([]byte(`{
+				"kind": "admin#directory#groups",
+				"groups": [{
+					"kind": "admin#directory#group",
+					"email": "group3@group.com"
+				}]
+			}`))
+		case "group3@group.com":
+			w.Write([]byte(`{"kind": "admin#directory#groups", "groups": []}`))
+		case "noGroupUser":
+			w.Write([]byte(`{"kind": "admin#directory#groups", "groups": []}`))
+		}
+	}))
+	defer groupsServer.Close()
+
+	type args struct {
+		user   string
+		config GSuiteProviderConfig
+	}
+	tests := []struct {
+		name     string
+		args     args
+		expected []string
+	}{
+		{
+			name: "fetch groups for user that's in no groups",
+			args: args{
+				user: "noGroupUser",
+				config: GSuiteProviderConfig{
+					serviceAccountKeyJSON:  []byte(serviceAccountKeyJSON),
+					ServiceAccountFilePath: "/path/to/google-service-account.json",
+					AdminImpersonateEmail:  "test@example.com",
+					FetchGroups:            true,
+				},
+			},
+			expected: []string{},
+		},
+		{
+			name: "fetch groups for group that's in no groups",
+			args: args{
+				user: "group3@group.com",
+				config: GSuiteProviderConfig{
+					serviceAccountKeyJSON:  []byte(serviceAccountKeyJSON),
+					ServiceAccountFilePath: "/path/to/google-service-account.json",
+					AdminImpersonateEmail:  "test@example.com",
+					FetchGroups:            true,
+				},
+			},
+			expected: []string{},
+		},
+		{
+			name: "fetch groups for user with default recursion max depth 0",
+			args: args{
+				user: "user1",
+				config: GSuiteProviderConfig{
+					serviceAccountKeyJSON:  []byte(serviceAccountKeyJSON),
+					ServiceAccountFilePath: "/path/to/google-service-account.json",
+					AdminImpersonateEmail:  "test@example.com",
+					FetchGroups:            true,
+				},
+			},
+			expected: []string{
+				"group1@group.com",
+			},
+		},
+		{
+			name: "fetch groups for user with recursion max depth 1",
+			args: args{
+				user: "user1",
+				config: GSuiteProviderConfig{
+					serviceAccountKeyJSON:  []byte(serviceAccountKeyJSON),
+					ServiceAccountFilePath: "/path/to/google-service-account.json",
+					AdminImpersonateEmail:  "test@example.com",
+					FetchGroups:            true,
+					GroupsRecurseMaxDepth:  1,
+				},
+			},
+			expected: []string{
+				"group1@group.com",
+				"group2@group.com",
+			},
+		},
+		{
+			name: "fetch groups for user with recursion max depth 10",
+			args: args{
+				user: "user1",
+				config: GSuiteProviderConfig{
+					serviceAccountKeyJSON:  []byte(serviceAccountKeyJSON),
+					ServiceAccountFilePath: "/path/to/google-service-account.json",
+					AdminImpersonateEmail:  "test@example.com",
+					FetchGroups:            true,
+					GroupsRecurseMaxDepth:  10,
+				},
+			},
+			expected: []string{
+				"group1@group.com",
+				"group2@group.com",
+				"group3@group.com",
+			},
+		},
+		{
+			name: "fetch groups for group with default recursion max depth 0",
+			args: args{
+				user: "group1@group.com",
+				config: GSuiteProviderConfig{
+					serviceAccountKeyJSON:  []byte(serviceAccountKeyJSON),
+					ServiceAccountFilePath: "/path/to/google-service-account.json",
+					AdminImpersonateEmail:  "test@example.com",
+					FetchGroups:            true,
+				},
+			},
+			expected: []string{
+				"group2@group.com",
+			},
+		},
+		{
+			name: "fetch groups for group with recursion max depth 1",
+			args: args{
+				user: "group1@group.com",
+				config: GSuiteProviderConfig{
+					serviceAccountKeyJSON:  []byte(serviceAccountKeyJSON),
+					ServiceAccountFilePath: "/path/to/google-service-account.json",
+					AdminImpersonateEmail:  "test@example.com",
+					FetchGroups:            true,
+					GroupsRecurseMaxDepth:  1,
+				},
+			},
+			expected: []string{
+				"group2@group.com",
+				"group3@group.com",
+			},
+		},
+		{
+			name: "fetch groups for group with recursion max depth 10",
+			args: args{
+				user: "group1@group.com",
+				config: GSuiteProviderConfig{
+					serviceAccountKeyJSON:  []byte(serviceAccountKeyJSON),
+					ServiceAccountFilePath: "/path/to/google-service-account.json",
+					AdminImpersonateEmail:  "test@example.com",
+					FetchGroups:            true,
+					GroupsRecurseMaxDepth:  10,
+				},
+			},
+			expected: []string{
+				"group2@group.com",
+				"group3@group.com",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Initialize the provider
+			gProvider := new(GSuiteProvider)
+			assert.NoError(t, gProvider.initialize(tt.args.config))
+
+			// Fetch groups from the groupsServer
+			ctx := context.Background()
+			gProvider.adminSvc, _ = admin.NewService(ctx, option.WithHTTPClient(&http.Client{}))
+			gProvider.adminSvc.BasePath = groupsServer.URL
+			groups := make(map[string]bool)
+			assert.NoError(t, gProvider.search(ctx, groups, tt.args.user, gProvider.config.GroupsRecurseMaxDepth))
+
+			// Assert that we got the expected groups
+			assert.Equal(t, len(tt.expected), len(groups))
+			for _, group := range tt.expected {
+				_, ok := groups[group]
+				assert.True(t, ok)
+			}
+		})
+	}
+}
 
 func TestGSuiteProvider_initialize(t *testing.T) {
 	type args struct {
